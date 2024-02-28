d6f85f643b797418913ce5f805b35de49d1e2611
Mariyam Iqbal <[valid-email]>
2024-02-28 07:56:25 -0800

    first commit

---
+++ ex1.js
@@ -0,0 +1,2 @@
+var personName = "mariyam";
+console.log(personName + ",would you like to learn some Python today");
---
+++ ex1.ts
@@ -0,0 +1,2 @@
+let personName: string = "mariyam"
+console.log(personName + ",would you like to learn some Python today")
